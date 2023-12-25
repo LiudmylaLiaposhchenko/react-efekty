ec1a2aa0b4fba877e171b8286c0a958914a7e369
LL <liaposhchenkol@dmail.com>
2023-12-25 20:58:21 +0100

    Ukol3

--- src/components/Ukol3/index.jsx
+++ src/components/Ukol3/index.jsx
@@ -3,7 +3,14 @@ Zadání: Vytvořte v této komponentě efekt, který pomocí funkce setTimeout 
   v konzoli vypíše text „Jsem tady“.
 */
 
+import { useEffect } from 'react';
+
 export const Ukol3 = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('Jsem tady');
+    }, 3000);
+  });
   return (
     <>
       <p>Jsem tady</p>
