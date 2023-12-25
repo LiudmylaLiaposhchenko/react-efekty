af8177797dae6d7bdfbe2106bb33c51113043a85
LL <liaposhchenkol@dmail.com>
2023-12-25 20:58:48 +0100

    Ukol5

--- src/components/Ukol5/index.jsx
+++ src/components/Ukol5/index.jsx
@@ -12,9 +12,22 @@ Krok 3: Pokud je stav `postava` `null`, nechte zobrazen text `Načítám…`. Po
 
 export const Ukol5 = () => {
   const [postava, setPostava] = useState(null);
+  useEffect(() => {
+    const fetchCharacter = async () => {
+      const response = await fetch(
+        'https://hp-api.onrender.com/api/character/ca3827f0-375a-4891-aaa5-f5e8a5bad225',
+      );
+      const data = await response.json();
+      setPostava(data);
+    };
+    fetchCharacter();
+  }, []);
+
   return (
     <>
-      <p>Načítám…</p>
+      <p>
+        {postava === null ? 'Načítám…' : `Jméno postavy: ${postava[0].name}`}
+      </p>
     </>
   );
 };
