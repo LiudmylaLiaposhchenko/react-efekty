ffe15347a1019abfac4f96423270282450f59b68
LL <liaposhchenkol@dmail.com>
2023-12-25 20:58:06 +0100

    Ukol2

--- src/components/Ukol2/index.jsx
+++ src/components/Ukol2/index.jsx
@@ -3,7 +3,12 @@ Zadání: Vytvořte v této komponentě efekt, který se spustí při prvním zo
   a v konzoli vypíše text „Jsem tady“.
 */
 
+import { useEffect } from 'react';
+
 export const Ukol2 = () => {
+  useEffect(() => {
+    console.log('Jsem tady');
+  }, []);
   return (
     <>
       <p>Jsem tady</p>
